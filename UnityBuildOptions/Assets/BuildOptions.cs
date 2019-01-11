a0d5a43523c4cea3e8b86fd87be563b5185b8dfe
Josef21296 <jcasanovasv@hotmail.com>
2019-01-11 20:04:13 +0100

    Build options asset

---
+++ UnityBuildOptions/Assets/BuildOptions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "BuildOptions", menuName = "BuildOptions")]
+public class BuildOptions : ScriptableObject
+{
+    [System.Serializable]
+    public class BuilOption
+    {
+        public BuildTargetGroup group = BuildTargetGroup.Unknown;
+        public string defines = "";
+
+        public BuilOption(BuildTargetGroup group)
+        {
+            this.group = group;
+            defines = "";
+        }
+    }
+
+    [HideInInspector] public List<BuilOption> m_buildOptions = new List<BuilOption>();
+
+    public string name = "";
+}
